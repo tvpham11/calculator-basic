d020ffb5134ab2393237d9d4b5455938cb646fb4
Vi Pham <tvpham11@gmail.com>
2015-05-12 16:04:37 -0400

    Completed JS and made #answer white.

---
+++ main.js
@@ -0,0 +1,14 @@
+var numOne = document.getElementById("num_one");
+var numTwo = document.getElementById("num_two");
+var submit = document.getElementById("calculateBtn");
+var answer = document.getElementById("answer");
+
+function add() {
+  var numberOne = Number(numOne.value);
+  var numberTwo = Number(numTwo.value);
+  var result = numberOne + numberTwo;
+  answer.innerHTML = result.toString();
+}
+
+
+submit.addEventListener("click", add);
